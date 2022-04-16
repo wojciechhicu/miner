00bdb2b5054641c259e38f7d85fb1eb562a407ef
wojciechhicu <trustme@onet.pl>
2022-04-16 02:37:30 +0200

    removed webworker

--- miner/src/app/components/dashboard/settings/settings.component.ts
+++ miner/src/app/components/dashboard/settings/settings.component.ts
@@ -142,20 +142,6 @@ export class SettingsComponent implements OnInit {
       }
     }
   }
-  workerTask() {
-
-    if (typeof Worker !== 'undefined') {
-      // Create a new
-      const worker = new Worker(new URL('../../../.worker', import.meta.url));
-      worker.onmessage = ({ data }) => {
-        console.log(`page got message: ${data.block}`);
-      };
-      worker.postMessage('hello');
-    } else {
-      // Web Workers are not supported in this environment.
-      // You should add a fallback so that your program still executes correctly.
-    }
-  }
   getWallets(): void {
     this.db.readUserWallet().snapshotChanges().pipe(
       map(changes => changes.map(c => ({
